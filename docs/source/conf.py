9e41d5c4d21acb3f25df6c3ee433dcf7a447d28f
sanirasheed <64389059+sanirasheed@users.noreply.github.com>
2023-03-15 10:08:07 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,18 +30,16 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = [
-     # ...
-   # 'sphinx_copybutton'
-   # ...
-  #  ]
+
+extensions = ["sphinx.ext.intersphinx", "sphinx_copybutton"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 docutils = ['_docutils']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------

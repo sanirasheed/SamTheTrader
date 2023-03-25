6af8e98ebf770740468ddd600a9c51c8716c9175
sanirasheed <64389059+sanirasheed@users.noreply.github.com>
2023-03-25 12:23:16 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ["sphinx_copybutton"]
+extensions = ["sphinx_copybutton","breathe"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
